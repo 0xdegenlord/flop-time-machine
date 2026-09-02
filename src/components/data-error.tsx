14e63aa13f52e99b6a040f503d4046898ca43b8e
Degen Lord <nk10004056@gmail.com>
2026-09-02 16:57:59 +0600

    Delete src directory

--- src/components/data-error.tsx
+++
@@ -1,8 +0,0 @@
-export function DataError({ message }: { message: string }) {
-  return (
-    <div className="data-error" role="alert">
-      <strong>Archive connection unavailable</strong>
-      <p>{message}</p>
-    </div>
-  );
-}
